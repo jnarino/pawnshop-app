5404ece9d95a24c06306190ab1b4651a231b2bf3
jnarino <jnarino@eafit.edu.co>
2025-08-09 11:35:33 -0400

    wip:started with user role view

--- client/electron/main.ts
+++ client/electron/main.ts
@@ -42,7 +42,7 @@ function createMainWindow() {
         },
         {
             label: 'About',
-            click: () => {
+            click: () => {  
                 dialog.showMessageBox(mainWindow, {
                     type: 'info',
                     title: 'About',
