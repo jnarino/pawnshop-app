fdeb19800b207cc24e3e2da3eedeb5a14b91d311
jnarino <jnarino@eafit.edu.co>
2025-08-25 09:54:15 -0400

    wip:version working with categories

--- server/dist/infrastructure/persistence/CategoryRepository.js
+++ server/dist/infrastructure/persistence/CategoryRepository.js
@@ -5,7 +5,7 @@ const db_1 = require("../db");
 const sqlLoader_1 = require("../db/sqlLoader");
 class CategoryRepository {
     async listFlat() {
-        const sql = (0, sqlLoader_1.getSQL)('query', 'inventory', 'categoriesTree');
+        const sql = (0, sqlLoader_1.getSQL)('query', 'inventory', 'listcategoriesTree');
         const { rows } = await db_1.pool.query(sql);
         return rows;
     }
